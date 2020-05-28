927a853ed17508586a7d24c8a6fae3876aa61b65
Aaftakhov <simple1elf@gmail.com>
2020-05-28 10:59:44 +0500

    first

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			
+		}
+	}
+}
